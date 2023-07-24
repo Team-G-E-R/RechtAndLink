8c64eb7dd361c1e0f4ec875b07e80af23b815bef
Zolotarev Evgenii Vladislavovich <xca0@yandex.ru>
2023-07-24 16:33:00 +0300

    scene 11 in work

--- Assets/Scripts/Cospero/Dialogue system/DialogueTrigger.cs
+++ Assets/Scripts/Cospero/Dialogue system/DialogueTrigger.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
+    [SerializeField] GameObject player;
     public DialogueWindow dialogue;
+    public bool needToOffControl;
     private bool DiaPlaying;
-   
+
     public void ActivateDialogue()
-    {   
-        DiaPlaying=FindObjectOfType<DialogueManagerCutscene>().dialogueIsPlaying;
-        if(DiaPlaying==false)
+    {
+        if (needToOffControl == true)
         {
-            FindObjectOfType<DialogueManagerCutscene>().StartDialogue(dialogue);
+            player.GetComponent<movecontr>().enabled = false;
+            player.transform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            DiaPlaying = FindObjectOfType<DialogueManagerCutscene>().dialogueIsPlaying;
+            if (DiaPlaying == false)
+            {
+                FindObjectOfType<DialogueManagerCutscene>().StartDialogue(dialogue);
+            }
         }
-          
+        else if (needToOffControl == false)
+        {
+            DiaPlaying = FindObjectOfType<DialogueManagerCutscene>().dialogueIsPlaying;
+            if (DiaPlaying == false)
+            {
+                FindObjectOfType<DialogueManagerCutscene>().StartDialogue(dialogue);
+            }
+        }
+
     }
 
     
